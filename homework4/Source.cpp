5aa9d75008df09e733bbb41e8b2a78cb37824873
Beomjoon Chae <cbj000921@snu.ac.kr>
2020-04-11 18:59:13 +0900

    changed due to my answer, lab2-04 - (1)

--- homework4/Source.cpp
+++ homework4/Source.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <time.h>
+#include <cstdlib>
+
 using namespace std;
 
 
@@ -20,14 +22,22 @@ int main()
 	//Given the int array arr, generate another int array output.
 	//whose element indiciates how many elements in arr is smaller than arr[i].
 	//For example, if arr is given as [5,8,5,6,8,1,5,9,5,8], 
-	//output should be [1,6,1,5,6,0,1,9,1,6]. 
+	//output should be [1,6,1,5,6,0,1,9,1,6].
 
 	int* output = new int[arrsize];
 	cout << "Array output: ";
 
-	/***********************************
-	Implement the code here!
-	************************************/
+	// My answer
+	//************************************
+	for (int i = 0; i < 10; i++) {
+		int count = 0;
+		for (int j = 0; j < 10; j++) {
+			if (arr[i] > arr[j])
+				count++;
+		}
+		output[i] = count;
+	}
+	//************************************
 
 	for (int i = 0; i < arrsize; i++) {
 		cout << output[i] << " ";
