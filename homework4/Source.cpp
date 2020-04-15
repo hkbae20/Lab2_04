f33904839241b2164018e96ba2156c01d27791cc
seohyun <justsally@snu.ac.kr>
2020-04-15 15:48:13 +0900

    cpp file complete

--- homework4/Source.cpp
+++ homework4/Source.cpp
@@ -25,9 +25,14 @@ int main()
 	int* output = new int[arrsize];
 	cout << "Array output: ";
 
-	/***********************************
-	Implement the code here!
-	************************************/
+	for (int i = 0; i < arrsize; i++) {
+		int count = 0;
+		for (int j = 0; j < arrsize; j++) {
+			if (arr[j] < arr[i])
+				count++;
+		}
+		output[i] = count;
+	}
 
 	for (int i = 0; i < arrsize; i++) {
 		cout << output[i] << " ";
